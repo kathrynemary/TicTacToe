a852310fbc0e81f5d36c6734e0cf5c3740881aa9
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-02-23 15:21:47 -0600

    will pick winning spot

--- Intelligence.rb
+++ Intelligence.rb
@@ -5,31 +5,29 @@ class Intelligence
 
   def self.choose_move(board)
     @board = board
-    if @board.exclude?(0) && @board.exclude?(1)
-      if @board.available_spaces.has_key? 2
-        return 2
-      else
-        if @board.available_spaces.has_key? 4
-          return 4
-        end
+
+    sets = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+    sets.each_index {|x|
+      @a = (@board.available_spaces.has_key? sets[x][0])
+      @b = (@board.available_spaces.has_key? sets[x][1])
+      @c = (@board.available_spaces.has_key? sets[x][2])
+      if @a && !@b && !@c
+        return sets[x][0]
+      elsif @b && !@a && !@c
+        return sets[x][1]
+      elsif @c && !@a && !@b
+        return sets[x][2]
       end
-    else
-      if @board.available_spaces.has_key? 4
-        return 4
-      end
-    end
+    }
+    pick_middle
   end
 
-  # def self.almost_winning
-  #   if @board.exclude?(0) && @board.exclude?(1)
-  #     if @board.available_spaces.has_key? 2
-  #       return 2
-  #     else
-  #       false
-  #     end
-  #   else
-  #     false
-  #   end
-  # end
+  def self.pick_middle
+    if @board.available_spaces.has_key? 4
+      return 4
+    else
+      return "I give up."
+    end
+  end
 
 end
--- spec/intelligence_spec.rb
+++ spec/intelligence_spec.rb
@@ -2,6 +2,8 @@
 #pass in an instance of board
 #for current board, return best move
 
+#will eventually need to distinguish between the 2 players' moves
+
 require_relative '../intelligence.rb'
 require_relative '../game.rb'
 require_relative '../board.rb'
@@ -25,6 +27,29 @@ describe Intelligence do
     @x.pick("y", 0)
     @x.pick("y", 1)
     expect(Intelligence.choose_move(@x)).to eq(2)
+  end
+
+  it "should pick the third to complete the second row" do
+    @x.pick("y", 3)
+    @x.pick("y", 4)
+    expect(Intelligence.choose_move(@x)).to eq(5)
+  end
+
+  it "should pick the third to complete the third row" do
+    @x.pick("y", 6)
+    @x.pick("y", 7)
+    expect(Intelligence.choose_move(@x)).to eq(8)
+  end
+
+  it "should coomplete a diagonal match" do
+    @x.pick("y", 0)
+    @x.pick("y", 8)
+    expect(Intelligence.choose_move(@x)).to eq(4)
+  end
+
+  it "shouldn't do anything if 4 is the only spot taken" do
+    @x.pick("y", 4)
+    expect(Intelligence.choose_move(@x)).to eq("I give up.")
   end
 
 end
