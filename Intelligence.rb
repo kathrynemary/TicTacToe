ba1822c97d7f4c4547a69ffd238f5ee78761904a
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-04-13 16:18:58 -0500

    Will work correctly at 1 level.

--- Intelligence.rb
+++ Intelligence.rb
@@ -2,49 +2,63 @@ require './board'
 
 class Intelligence
 
-  def self.choose_move(symbol, board)
+  def initialize(symbol, other_symbol, board)
     @symbol = symbol
+		@other_symbol = other_symbol
 		@board = board
+	end
+
+  def choose_move
 		minimax(@board, 0)
 	  @selection	
 	end
   
-	def self.calculate_score
-    if @board.winner?(@symbol) #it won't know this
-			(10 - @levels.to_i)
-		elsif @board.winner?(other_symbol)
-		 puts "how is it even doing this?"	#get other_score
+	def calculate_score
+    if @board.winner?(@symbol) 
+		  (10 - @levels.to_i)
+		elsif @board.winner?(@other_symbol)
 			(@levels.to_i - 10)
 		else
 			0
 		end
   end
 
-  def self.minimax(board, level)
-    available = @board.available_spaces
+  def minimax(board, level)
+		@scores = []
+		@moves = []
+  	#until @board.winner?(@symbol)
+      available = @board.available_spaces.keys
+      @levels = level
 
-  	if @board.winner?(@symbol)
-			calculate_score
-			@levels = @levels.to_i + 1
-			scores = []
-			moves = []
+      available.each do |key|
+				@board.board[key] = @symbol
+				#@levels += 1
+	      if @board.winner?(@symbol)  
+					@selection = key
+					break
+				else
+          @board.board[key] = key #this knows way too much but YOLO!
+				end
+			end
 		end
+				
+				#	  @scores.push(possible_move)	#minimax(possible, @levels)
+		 # 	puts "updated scores: #{@scores}"	
+		#		@moves.push(possible_move)
+		#		puts "updated moves: #{@moves}"
+ 
+    #calculate_score
 
-		puts available
-    possible = available.each_key { |key| @board.pick(@symbol, key)  }   
-			scores.push	minimax(possible, @levels)
-			moves.push space
-
-   # if @game.play_a_turn(symbol) 
-      #max_score = scores.each_with_index.max[1]
-			#@selection = moves[max_score]
-			#scores[max_score]
-		#else
-		#	min_score = scores.each_with_index.min[1]
-		#	@selection = moves[min_score]
-		#	scores[min_score]
+		#if @symbol 
+#			max_score = 2 #@scores.max
+#			@selection = "2" #@moves[max_score]
+			 #@scores[max_score]	
+			#else
+				#min_score = @scores.min
+				#@selection = @moves[min_score]
+				#@scores[min_score]
+			  #min_score
 		#end
-	end
 
 end
 
--- spec/intelligence_spec.rb
+++ spec/intelligence_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../intelligence.rb'
-require_relative '../board.rb'
 
 describe Intelligence do
 
@@ -8,55 +7,57 @@ describe Intelligence do
   end
 =begin
   it "should pick the middle space if it is available" do
-    expect(Intelligence.choose_move("r", @x)).to eq('4')
+    expect(Intelligence.choose_move("r", "h", @x)).to eq('4')
   end
 =end
   it "should not pick 4 when 4 is already taken" do
     @x.pick("P", '4')
-    expect(Intelligence.choose_move("K", @x)).not_to eq('4')
+    test = Intelligence.new("K", "P", @x)
+		expect(test.choose_move).not_to eq('4')
   end
-=begin
+
   it "should pick the third to complete the first row" do
     @x.pick("y", "0")
     @x.pick("y", "1")
-    expect(Intelligence.choose_move("y", @x)).to eq('2')
+		test = Intelligence.new("y", "u", @x)
+    expect(test.choose_move).to eq('2')
   end
-=begin
+
   it "should pick the third to complete the second row" do
     @x.pick("y", '3')
     @x.pick("y", '4')
-    expect(Intelligence.choose_move("y", @x)).to eq('5')
+		test = Intelligence.new("y", "j", @x)
+    expect(test.choose_move).to eq('5')
   end
 
   it "should pick the third to complete the third row" do
     @x.pick("y", '6')
     @x.pick("y", '7')
-    expect(Intelligence.choose_move("y", @x)).to eq('8')
+    test = Intelligence.new("y", "h", @x)
+		expect(test.choose_move).to eq('8')
   end
 
   it "should complete a diagonal match" do
     @x.pick("y", '0')
     @x.pick("y", '8')
-    expect(Intelligence.choose_move("y", @x)).to eq('4')
+		test = Intelligence.new("y", "k", @x)
+    expect(test.choose_move).to eq('4')
   end
 
   it "should complete a vertical match" do
     @x.pick("y", '1')
     @x.pick("y", '7')
-    expect(Intelligence.choose_move("y", @x)).to eq('4')
+		test = Intelligence.new("y", "j", @x)
+    expect(test.choose_move).to eq('4')
   end
-
-  it "should pick an available spot if middle is taken" do
-    @x.pick("y", '4')
-    expect(Intelligence.choose_move("y", @x)).to eq('0')
-	end
 
   it "should complete the third row to win, rather than the 2nd row to avoid a loss" do
     @x.pick("g", '3')
     @x.pick("g", '4')
     @x.pick("y", '6')
     @x.pick("y", '7')
-    expect(Intelligence.choose_move("y", @x)).to eq('8')
+    test = Intelligence.new("y", "g", @x)
+		expect(test.choose_move).to eq('8')
   end
 
   it "should complete the first row to win, rather than the 3rd row to avoid a loss" do
@@ -64,7 +65,8 @@ describe Intelligence do
     @x.pick("g", '8')
     @x.pick("y", '0')
     @x.pick("y", '2')
-    expect(Intelligence.choose_move("y", @x)).to eq('1')
+    test = Intelligence.new("y", "g", @x)
+		expect(test.choose_move).to eq('1')
   end
   
 	it "should complete the second row to win, rather than the 1st row to avoid a loss" do
@@ -72,7 +74,8 @@ describe Intelligence do
     @x.pick("g", '1')
     @x.pick("y", '4')
     @x.pick("y", '5')
-    expect(Intelligence.choose_move("y", @x)).to eq('3')
+		test = Intelligence.new("y", "g", @x)
+    expect(test.choose_move).to eq('3')
   end
 
 	it "should complete the 1st column to win, rather than 2nd col to avoid a loss" do
@@ -80,7 +83,8 @@ describe Intelligence do
     @x.pick("g", '4')
     @x.pick("y", '0')
     @x.pick("y", '3')
-    expect(Intelligence.choose_move("y", @x)).to eq('6')
+		test = Intelligence.new("y", "g", @x)
+    expect(test.choose_move).to eq('6')
   end	
 	
 	it "should complete the 2nd column to win, rather than 3rd column to avoid a loss" do
@@ -88,7 +92,8 @@ describe Intelligence do
     @x.pick("g", '2')
     @x.pick("y", '1')
     @x.pick("y", '4')
-    expect(Intelligence.choose_move("y", @x)).to eq('7')
+		test = Intelligence.new("y", "g", @x)
+    expect(test.choose_move).to eq('7')
   end
 	
 	it "should complete the 3rd column to win, rather than 1st column to avoid a loss" do
@@ -96,7 +101,8 @@ describe Intelligence do
     @x.pick("g", '3')
     @x.pick("y", '8')
     @x.pick("y", '5')
-    expect(Intelligence.choose_move("y", @x)).to eq('2')
+		test = Intelligence.new("y", "g", @x)
+    expect(test.choose_move).to eq('2')
   end
 	
 	it "should complete the descending diagonal to win, rather than avoid a loss" do
@@ -104,7 +110,8 @@ describe Intelligence do
     @x.pick("g", '2')
     @x.pick("y", '4')
     @x.pick("y", '0')
-    expect(Intelligence.choose_move("y", @x)).to eq('8')
+		test = Intelligence.new("y", "g", @x)
+    expect(test.choose_move).to eq('8')
   end
 	
 	it "should complete the ascending diagonal to win, rather than avoid a loss" do
@@ -112,7 +119,8 @@ describe Intelligence do
     @x.pick("g", '1')
     @x.pick("y", '4')
     @x.pick("y", '2')
-    expect(Intelligence.choose_move("y", @x)).to eq('6')
+    test = Intelligence.new("y", "g", @x)
+		expect(test.choose_move).to eq('6')
   end
-=end
+
 end
