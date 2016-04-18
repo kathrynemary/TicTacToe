0bd69944a45a4f8ee8bb3d64c99991bf2bfa5050
Kathryne Dunlap <kathryne@Kathrynes-MacBook-Pro.local>
2016-04-18 13:08:22 -0500

    Updated player class and tests.

--- player.rb
+++ player.rb
@@ -3,7 +3,6 @@ require_relative './board'
 class Player
 
   def initialize 
-    @score = 0
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,25 +2,5 @@ require_relative '../player'
 
 describe Player do
 
-=begin
-  it "should return a symbol for the player" do
-    expect(Player.new("X").symbol).to eq("X")
-  end
-
-  it "should allow a different symbol for another player" do
-    expect(Player.new("Z").symbol).to eq("Z")
-  end
-=end
-
-  it "should return the score" do
-    expect(Player.new.score).to eq(0)
-  end
-
-  it "should update the score twice" do
-    example = Player.new
-    example.update_score
-    example.update_score
-    expect(example.score).to eq(2) 
-  end
-
+	#these test have been moved to game_builder_symbol_interface_spec
 end
